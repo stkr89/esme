9c09714eda783f1c178196475dc7960db02e188c
Sumit Tokkar <stokkar@confluent.io>
2021-02-12 16:42:24 -0600

    enable cors

--- handler.go
+++ handler.go
@@ -10,18 +10,20 @@ import (
 
 var routeConfigMap map[string]*route
 
-func enableCors(w *http.ResponseWriter) {
+func setupResponse(w *http.ResponseWriter, req *http.Request) {
 	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 }
 
 func handleAll(w http.ResponseWriter, req *http.Request) {
-	enableCors(&w)
+	setupResponse(&w, req)
 
 	if _, ok := routeConfigMap[getRouteMapKey(req.Method, req.URL.Path)]; !ok {
 		http.Error(w, "Not found", http.StatusNotFound)
 	}
 
-	if req.Method == http.MethodOptions {
+	if (*req).Method == http.MethodOptions {
 		return
 	}
 
